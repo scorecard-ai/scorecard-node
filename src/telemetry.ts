3887ea38623da297944c46532945562b0285f903
Nathan Hammond <nathanhammond@getscorecard.ai>
2024-06-11 17:15:57 +0800

    Add context propagation. (#20)

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,9 +1,3 @@
-const { trace } = require('@opentelemetry/api');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { Resource } = require('@opentelemetry/resources');
-const { BatchSpanProcessor, ConsoleSpanExporter, NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-
 function checkInstalled(...modules: string[]) {
   for (let i = 0; i < modules.length; i++) {
     let module = modules[i];
@@ -22,6 +16,26 @@ module.exports = {
     telemetryKey: string,
     telemetryUrl?: string,
   }, debug = false) {
+    const required = [
+      '@opentelemetry/api',
+      '@opentelemetry/core',
+      '@opentelemetry/exporter-trace-otlp-http',
+      '@opentelemetry/resources',
+      '@opentelemetry/sdk-trace-node',
+      '@opentelemetry/semantic-conventions'
+    ];
+
+    if (!checkInstalled(...required)) {
+      throw new Error(`Required OpenTelemetry packages not installed. Please ensure that all necessary peerDependencies are installed:\n${required.map(module => `- ${module}`).join('\n')}`);
+    }
+
+    const { propagation, trace } = require('@opentelemetry/api');
+    const { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } = require('@opentelemetry/core');
+    const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+    const { Resource } = require('@opentelemetry/resources');
+    const { BatchSpanProcessor, ConsoleSpanExporter, NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+    const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+
     const resource = Resource.default().merge(
       new Resource({
         [SEMRESATTRS_SERVICE_NAME]: name,
@@ -38,6 +52,13 @@ module.exports = {
       provider.addSpanProcessor(processor);
     }
 
+    propagation.setGlobalPropagator(new CompositePropagator({
+      propagators: [
+        new W3CTraceContextPropagator(),
+        new W3CBaggagePropagator()
+      ]
+    }));
+
     const url = scorecardConfig.telemetryUrl ? scorecardConfig.telemetryUrl : 'https://telemetry.getscorecard.ai';
     const otlpExporter = new OTLPTraceExporter({
       url: `${url}/v1/traces`,
@@ -50,12 +71,12 @@ module.exports = {
 
     provider.register();
 
-    if (checkInstalled('@langchain/core')) {
+    if (checkInstalled('@langchain/core', '@arizeai/openinference-instrumentation-langchain', '@opentelemetry/instrumentation')) {
       const { LangChainInstrumentation } = require('@arizeai/openinference-instrumentation-langchain');
-      new LangChainInstrumentation().manuallyInstrument(require('@langchain/core/callbacks/manager'));  
+      new LangChainInstrumentation().manuallyInstrument(require('@langchain/core/callbacks/manager'));
     }
 
-    if (checkInstalled('openai')) {
+    if (checkInstalled('openai', '@arizeai/openinference-instrumentation-openai', '@opentelemetry/instrumentation')) {
       const { OpenAIInstrumentation } = require('@arizeai/openinference-instrumentation-openai');
       new OpenAIInstrumentation().manuallyInstrument(require('openai'));
     }
